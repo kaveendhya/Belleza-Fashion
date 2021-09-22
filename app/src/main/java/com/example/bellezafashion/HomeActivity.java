e9002ceb7df539a00a249d5db73c15412923234d
miuninihara001 <miuniniharae@gmail.com>
2021-09-22 22:53:58 +0530

    commit23

---
+++ app/src/main/java/com/example/bellezafashion/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.bellezafashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button shop,designer,profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        shop = (Button) findViewById(R.id.hbtn1);
+        designer = (Button) findViewById(R.id.hbtn2);
+        profile = (Button) findViewById(R.id.hbtn3);
+
+         profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityProfile();
+
+            }
+        });
+    }
+        private void openActivityProfile() {
+        Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+}
